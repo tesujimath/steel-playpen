6619110f1f96e2373f21b69e0c4fb8e7c41003a1
Simon Guest <simon.guest@tesujimath.org>
2025-04-08 16:13:09 +1200

    fibonacci

--- tspl.scm
+++ tspl.scm
@@ -16,3 +16,19 @@
   (syntax-rules ()
     [(_ () e) e]
     [(_ ([n1 v1] [n2 v2] ...) e) (let ([n1 v1]) (let-seq ([n2 v2] ...) e))]))
+
+;; not tail call, also terribly inefficient
+(define fibonacci-bad
+  (lambda (n)
+    (let fib ([i n])
+      (cond
+        [(= i 1) 1]
+        [else (+ (fib (- i 1)) (fib (- i 2)))]))))
+
+;; tail call
+(define fibonacci
+  (lambda (n)
+    (let fib ([i n] [a1 1] [a2 0])
+      (cond
+        [(= i 1) a1]
+        [else (fib (- i 1) (+ a1 a2) a1)]))))
