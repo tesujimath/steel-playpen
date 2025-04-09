5464bbd9b82c88cb106f27b4a95ce0e8a726366c
Simon Guest <simon.guest@tesujimath.org>
2025-04-09 15:16:30 +1200

    dir-vec

--- dirtree.scm
+++ dirtree.scm
@@ -1,3 +1,5 @@
 (#%require-dylib "libsteel_playpen"
   (only-in dir-tree
+    ; dir-set
+    dir-vec
     DirTree-size))
--- src/dirtree.rs
+++ src/dirtree.rs
@@ -1,7 +1,13 @@
+use std::collections::HashSet;
+
 // Using ABI Stable types is very important
-use steel::steel_vm::{
-    ffi::{FFIModule, RegisterFFIFn},
-    register_fn::RegisterFn,
+use steel::{
+    rvals::IntoSteelVal,
+    steel_vm::{
+        ffi::{FFIModule, RegisterFFIFn},
+        register_fn::RegisterFn,
+    },
+    SteelVal,
 };
 use steel_derive::Steel;
 
@@ -48,7 +54,42 @@ fn dir_tree(path: String) -> DirTree {
     )
 }
 
+fn dir_iter(path: String) -> impl Iterator<Item = String> {
+    let read_dir =
+        std::fs::read_dir(&path).unwrap_or_else(|e| panic!("failed to read {}: {}", &path, e));
+
+    read_dir.map(|entry| {
+        entry
+            .unwrap_or_else(|e| panic!("failed on entry : {}", e))
+            .file_name()
+            .to_string_lossy()
+            .into_owned()
+    })
+}
+
+fn dir_set(path: String) -> HashSet<String> {
+    dir_iter(path).collect::<HashSet<String>>()
+}
+
+fn dir_vec(path: String) -> Vec<String> {
+    dir_iter(path).collect::<Vec<String>>()
+}
+
+fn consume_dir<D>(d: D)
+where
+    D: IntoSteelVal,
+{
+}
+
+fn consume_dir_from_path(path: String) {
+    let d = dir_set(path);
+    consume_dir(d);
+}
+
 pub fn register_fns(module: &mut FFIModule) {
     module.register_fn("dir-tree", dir_tree);
     module.register_fn("DirTree-size", DirTree::size);
+
+    // module.register_fn("dir-set", dir_set);
+    module.register_fn("dir-vec", dir_vec);
 }
